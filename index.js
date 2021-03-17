6af4121f781f9a23f73f7c97ba772eaff4663450
Andres <andresjml@gmail.com>
2021-03-17 17:34:35 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+const employee = {
+    name:'Andres', StreetAddress: '16520 Evolve Dr.'
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = {...obj };
+
+    newObj[key] = value;
+
+    return newObj;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    
+    obj[key]=value;
+
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key){
+    const newObj = {...obj};
+    
+    delete newObj.name;
+
+    return newObj;
+};
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    
+    delete obj.name;
+
+    return obj;
+}
+
